2d3a096f66596c8cb43613d2fb38c878916d2bd4
BatalinVlad <vladbatalinontech@gmail.com>
2024-03-14 14:45:17 +0200

    Update Navigator.jsx

--- client/src/components/Navigator.jsx
+++ client/src/components/Navigator.jsx
@@ -4,7 +4,6 @@ const Navigator = () => {
   return (
     <nav className='main-nav'>
       <ul className='flex justify-evenly'>
-        <li><a href="#about">מי אני</a></li>
         <li><a href="#notes">רשמו עלי</a></li>
       </ul>
     </nav>
