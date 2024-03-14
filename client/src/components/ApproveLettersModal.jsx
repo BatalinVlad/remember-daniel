3a805440226a6929ca3a47832f766909a3824ece
BatalinVlad <vladbatalinontech@gmail.com>
2024-03-14 15:37:26 +0200

    styles

--- client/src/components/ApproveLettersModal.jsx
+++ client/src/components/ApproveLettersModal.jsx
@@ -10,7 +10,7 @@ function ApproveLettersModal({
     e.stopPropagation();
     try {
       const response = await fetch(
-        `http://localhost:3001/api/letters/${letterData.id}`,
+        `https://remember-daniel-production.up.railway.app/api/letters/${letterData.id}`,
         {
           method: "PUT",
           headers: {
